dd798e8fbedb8dafb35062f4a9c7514d8804db66
Chan Kwon <kwon11@ucla.edu>
2017-03-11 14:08:36 -0800

    outline basic Results page and components

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import Header from './Header.jsx';
 import Profile from './Profile.jsx'
 
 import ResultPage from './ResultPage.jsx';
 
+=======
+import ResultPage from './ResultPage.jsx';
+>>>>>>> outline basic Results page and components
 
 class App extends React.Component {
   constructor(props) {
@@ -14,8 +18,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
+<<<<<<< HEAD
         <Header />
         <Profile />
+=======
+>>>>>>> outline basic Results page and components
         <ResultPage />
       </div>
     );
