7cdce57e7de870a3bc42353012da58d84b90c309
efesurekli <e.surekli@gmail.com>
2017-03-11 14:33:33 -0800

    Add webhook for the chatbot

--- serverless/handler.js
+++ serverless/handler.js
@@ -14,3 +14,14 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.webhook = (event, context, callback) => {
+  if (event.method === 'GET') {
+   if (event.query['hub.verify_token'] === 'openToken' && event.query['hub.challenge']) {
+      return callback(null, parseInt(event.query['hub.challenge']));
+    } else {
+      return callback('Invalid token');
+    }
+  }
+}
+
