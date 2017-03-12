6673b2eb760cf33ec5c73a774f7ddd8086e4bcea
Kai Hsia <kaihsia@gmail.com>
2017-03-11 17:20:59 -0800

    debug1

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const chatHandler = require('./chat/handler.js').handler;
-const nutri = require('./api/nutriParse.js');
+// const nutri = require('./api/nutriParse.js');
 
 
 module.exports.index = (event, context, callback) => {
@@ -21,16 +21,16 @@ module.exports.index = (event, context, callback) => {
 };
 
 module.exports.webhook = (event, context, callback) => {
-    return chatHandler(event, context, callback);
-  }
-
-module.exports.run = (event, context) => {
-  const time = new Date();
-
-  console.log('Cron job running!');
-
+  return chatHandler(event, context, callback);
 };
 
-module.exports.nutriParser = (event, context, callback) => {
-  nutri(event, context, callback);
-};
+// module.exports.run = (event, context) => {
+//   const time = new Date();
+
+//   console.log('Cron job running!');
+
+// };
+
+// module.exports.nutriParser = (event, context, callback) => {
+//   nutri(event, context, callback);
+// };
