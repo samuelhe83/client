6627a5ccd9a930e2dc9264e7042eb7264b4bb84c
Samuel He <samuelhe83@gmail.com>
2017-03-12 14:38:54 -0700

    Work on dummy data

--- components/ResultOption.jsx
+++ components/ResultOption.jsx
@@ -9,34 +9,18 @@ class ResultOption extends React.Component {
     return (
       <div className="ResultOption">
         <div className="RestaurantAvatar">
-          <img src="./fakeRestaurant.jpg"></img>
+          <img src={this.props.restaurant.image}></img>
         </div>
         <div className="RestaurantInfo">
           <div className="RestaurantName restaurant">
             {this.props.value.name}
           </div>
-
-          <div className="RestaurantHours">
-            <div className="RestaurantOpen restaurant">
-              Open: {this.props.value.hours[0]}
-            </div>
-            <div className="RestaurantClose restaurant">
-              Close: {this.props.value.hours[1]}
-            </div>
-          </div>
-
-
-          <div className="RestaurantRating restaurant">
-            {this.props.value.rating}
-          </div>
-
-          <div className="RestaurantTags">
-            {this.props.value.tags.map(function(tag, index) {
-              return <li key={index}>{tag}</li>
-            })}
+        </div>
+        <div className="RestaurantInfo">
+          <div className="RestaurantSeats restaurant">
+            {this.props.value.seats}
           </div>
         </div>
-
       </div>
     )
   }
