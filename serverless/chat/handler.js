b76f4fceff3bc96e1318d03312f13d5f7dee9abe
efesurekli <e.surekli@gmail.com>
2017-03-11 16:35:20 -0800

    Set up dynamodb

--- serverless/chat/handler.js
+++ serverless/chat/handler.js
@@ -1,9 +1,20 @@
-const accessToken = 'EAAEvFFNRaBMBADq6JBuf2hcZCZC2HOeZAEuoPfJCUMeNPKxPZBvKByjFucL1vmkakhcdJIVH8je8XQBI7cqXPJm9c6CuGF9TK77NMtcopEWjN8nJqdOzlPuIsJuH4PZCk1UQHeqD6COKJ3LZBwAHkRurJoKLz1GIFfTgzocstFZAAZDZD';
+const config = require('config');
+const pageAccessToken = config.get('pageAccessToken');
+const validationToken = config.get('validationToken');
 const axios = require('axios');
+
+// if you need to add conversational elements: 
+// const { Wit, log } = require('node-wit');
+// const WIT_TOKEN = ..
+// const wit = new Wit({
+//   accessToken: WIT_TOKEN,
+//   actions,
+//   logger: new log.Logger(log.INFO),
+// });
 
 module.exports.handler = (event, context, callback) => {
   if (event.method === 'GET') {
-      if (event.query['hub.verify_token'] === 'openToken' && event.query['hub.challenge']) {
+      if (event.query['hub.verify_token'] === validationToken && event.query['hub.challenge']) {
         return callback(null, parseInt(event.query['hub.challenge']));
       } else {
         return callback('Invalid token');
@@ -14,7 +25,8 @@ module.exports.handler = (event, context, callback) => {
       event.body.entry.map((entry) => {
       entry.messaging.map((messagingItem) => {
         if (messagingItem.message && messagingItem.message.text) {
-          const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`;
+          // a dummy message to greet:          
+          const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${pageAccessToken}`;
           const payload = {
             recipient: {
               id: messagingItem.sender.id
